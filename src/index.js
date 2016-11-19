a3229d47be90837dd5752ce2b58ec0994ce1d0fe
Maicon Marioto <maicon.marioto@gmail.com>
2016-11-19 17:30:15 -0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyB99MG77uTj-pBS5zW6oVgeF69dRGlEa_c';
+
+class App extends Component {
+   constructor(props){
+      super();
+      this.state = {
+         videos: [],
+         selectedVideo: null
+      };
+      this.videoSearch('fail army');
+   }
+
+   videoSearch(term) {
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+         this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+         });
+      });
+   }
+
+   render() {
+      const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+      return (
+         <div>
+            <SearchBar onSearchTermChange={videoSearch} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+               onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+               videos={this.state.videos} />
+         </div>
+      );
+   }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
